def081cf6f3b07a75537674ffdeea37aba459a53
Muhammad Shakeel Anjum <viberaytech@Muhammads-MacBook-Air.local>
2026-08-04 00:32:20 +0500

    chore: Update bundle identifier and package name to com.viberaytech.reverseimage

--- android/app/src/main/java/com/viberaytech/reverseimage/MainActivity.kt
+++ android/app/src/main/java/com/viberaytech/reverseimage/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.viberay.reverseimagesearchapp
+package com.viberaytech.reverseimage
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/viberaytech/reverseimage/MainApplication.kt
+++ android/app/src/main/java/com/viberaytech/reverseimage/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.viberay.reverseimagesearchapp
+package com.viberaytech.reverseimage
 
 import android.app.Application
 import android.content.res.Configuration
